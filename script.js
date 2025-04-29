3b91ccf6659cc4bbc508b60e42c43afbf67679aa
Dark1200p <nameedark@gmail.com>
2025-04-30 00:52:54 +0300

    Commit message

---
+++ script.js
@@ -0,0 +1,17 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: #111;
+  color: #fff;
+  text-align: center;
+  margin-top: 100px;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #0d6efd;
+  color: white;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 8px;
+}
